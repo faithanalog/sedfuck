3d9255a82f762065a9755d74aba2fc7d2abfe873
Faith Analog <faith@faithanalog.com>
2018-12-01 02:28:35 -0800

    Slight cleanup

--- brainfuck.sh
+++ brainfuck.sh
@@ -1,8 +1,7 @@
+#!/bin/sh
 set -e
-
-
 cat brainfuck.sed.m4 | sed '
     s/\[\]/\\([^<>]*<>\\)/g
     s/<>/<>/g
 ' | tee temp.m4 | m4 | grep '[^ ]' > brainfuck.sed
-cat helloworld.bf | sed -n -f brainfuck.sed 
+cat "$1" /dev/stdin | sed -n -f brainfuck.sed
