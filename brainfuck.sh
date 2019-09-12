e3db6a0aa52c4af4ff29090e8fd9ee2aa66cf0d7
artemis <mail@artemis.sh>
2019-09-12 14:52:37 -0700

    minimize

--- brainfuck.sh
+++ brainfuck.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 set -e
 
+minimize=1
+if [ "$1" = "-M" ]; then
+    minimize=0
+    shift
+fi
+
 if command -v m4 2>&1 1>/dev/null && [ -f brainfuck.sed.m4 ]; then
     # I need to use grep -a because grep detects my sed file as a binary file...
-    cat brainfuck.sed.m4 | sed '
-        s/\[\]/\\([^<>]*<>\\)/g
-    ' | tee temp.m4 | m4 | grep -a '[^ ]' > brainfuck.sed
+    if [ $minimize -eq 1 ]; then
+        cat brainfuck.sed.m4 | sed '
+            s/\[\]/\\([^<>]*<>\\)/g
+        ' | tee temp.m4 | m4 | grep -a '[^ ]' | grep -a -v '^ *#' > brainfuck.sed
+    else
+        cat brainfuck.sed.m4 | sed '
+            s/\[\]/\\([^<>]*<>\\)/g
+        ' | tee temp.m4 | m4 | grep -a '[^ ]' > brainfuck.sed
+    fi
 fi
 
 if [ $# -eq 0 ]; then
